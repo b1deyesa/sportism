18b2c9fc9935298847471cb6ae9c9a7c454e886c
= <=>
2026-02-21 12:34:31 +0700

    Auto Commit

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css">
 
     {{-- Script --}}
-    {{-- <script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script> --}}
+    <script src="https://cdn.ckeditor.com/4.22.1/standard/ckeditor.js"></script>
     <script src="https://kit.fontawesome.com/4419d23bf4.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"></script>
     
